72c3a77b0f4786bdab932b081b2e81983dfa3c01
Samher <17sahe@stockholmscience.se>
2020-03-13 09:52:27 +0100

    logic

--- Mandelbrot/Form1.cs
+++ Mandelbrot/Form1.cs
@@ -16,12 +16,29 @@ namespace Mandelbrot
         public Form1()
         {
             InitializeComponent();
+            Complex result = new Complex(0,0);
+            Complex nResult;
 
+            for (double b = -1; b <= 1; b += 2 / 500)
+            {
+                for (double a = -2.5; a <= 1; a += 3.5 / 750)
+                {
+                    Complex c = new Complex(a, b);
+
+                    for (int n = 0; n < 1000; n++)
+                    {
+                        nResult = f(result, c);
+                        if (Complex.Abs(nResult) > 2)
+                            break;
+                        if (Complex.Abs(nResult) < 2 && n == 999) { }
+                            //set pixel color
+                    }
+                }
+            }
         }
 
         public Complex f(Complex z, Complex c)
         {
-            
             return z * z + c;
         }
     }

871441d036373b16104d9f878bbe9e0e3612b4c7
Samher <17sahe@stockholmscience.se>
2020-03-13 10:33:14 +0100

    ???

--- Mandelbrot/Form1.cs
+++ Mandelbrot/Form1.cs
@@ -17,13 +17,22 @@ namespace Mandelbrot
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private Complex f(Complex z, Complex c)
+        {
+            return z * z + c;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             Graph = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            Complex result = new Complex(0,0);
+            Complex result = new Complex(0, 0);
             Complex nResult;
 
-            for (double b = -1; b <= 1; b += 2 / 500)
+            for (double b = -1; b <= 1; b += 2 / pictureBox1.Height)
             {
-                for (double a = -2.5; a <= 1; a += 3.5 / 750)
+                for (double a = -2.5; a <= 1; a += 3.5 / pictureBox1.Width)
                 {
                     Complex c = new Complex(a, b);
 
@@ -32,16 +41,29 @@ namespace Mandelbrot
                         nResult = f(result, c);
                         if (Complex.Abs(nResult) > 2)
                             break;
-                        if (Complex.Abs(nResult) < 2 && n == 999) { }
-                            //set pixel color
+                        if (Complex.Abs(nResult) < 2 && n == 999)
+                        {
+                            double aCoord = (a / 3.5) * pictureBox1.Width + (2.5 / 3.5) * pictureBox1.Width;
+                            double bCoord = ((b / 2) * pictureBox1.Height + pictureBox1.Height / 2);
+                            Graph.SetPixel((int)aCoord, (int)bCoord, Color.Black);
+                            pictureBox1.Image = Graph;
+                        }
                     }
                 }
             }
         }
 
-        private void Form1_ResizeEnd(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            return z * z + c;
+            Bitmap bah = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    bah.SetPixel(x, y, Color.Black);
+                    pictureBox1.Image = bah;
+                }
+            }
         }
     }
 }

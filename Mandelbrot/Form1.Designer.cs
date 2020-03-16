ed001ae767485a0cae096b7eb7fb16bb6f9382cd
Adam Magnusson <admag2001@gmail.com>
2020-03-16 19:54:22 +0100

    Lillefönster

--- Mandelbrot/Form1.Designer.cs
+++ Mandelbrot/Form1.Designer.cs
@@ -28,39 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // button1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(740, 443);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "mandelbrot me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(741, 445);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(120, 25);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.Opacity = 0.5D;
             this.Text = "Form1";
-            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
-            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Mandelbrot/Form1.cs
+++ Mandelbrot/Form1.cs
@@ -14,11 +14,9 @@ namespace Mandelbrot
     public partial class Form1 : Form
     {
         Bitmap Graph;
-        Size currentSize;
         public Form1()
         {
             InitializeComponent();
-            Calculate();
         }
 
         private Complex f(Complex z, Complex c)
@@ -28,11 +26,15 @@ namespace Mandelbrot
 
         private void Calculate()
         {
-            Graph = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graph = new Bitmap(10500, 6000);
 
-            for (double b = -1; b <= 1; b += 2f / pictureBox1.Height)
+            for (int y = 0; y < Graph.Height; y++)
+                for (int x = 0; x < Graph.Width; x++)
+                    Graph.SetPixel(x, y, Color.White);
+
+            for (double b = -1; b <= 1; b += 2f / Graph.Height)
             {
-                for (double a = -2.5; a <= 1; a += 3.5 / pictureBox1.Width)
+                for (double a = -2.5; a <= 1; a += 3.5 / Graph.Width)
                 {
                     Complex z = new Complex(0, 0);
                     Complex c = new Complex(a, b);
@@ -42,23 +44,23 @@ namespace Mandelbrot
                         z = f(z, c);
                         n++;
                     }
-                    double aCoord = (a / 3.5) * pictureBox1.Width + (2.5 / 3.5) * pictureBox1.Width;
-                    double bCoord = ((b / 2) * pictureBox1.Height + pictureBox1.Height / 2);
-                    Graph.SetPixel((int)aCoord, (int)bCoord, Color.FromArgb(n / 4, 0, 0, 0));
-                    pictureBox1.Image = Graph;
+                    //if (Complex.Abs(z) < 2)
+                    //{
+                        double aCoord = (a / 3.5) * Graph.Width + (2.5 / 3.5) * Graph.Width;
+                        double bCoord = ((b / 2) * Graph.Height + Graph.Height / 2);
+                        Graph.SetPixel((int)aCoord, (int)bCoord, Color.FromArgb(255, 255 - n / 4, 255 - n / 4, 255 - n / 4));
+                    //}
                 }
             }
+            Graph.Save("GrandeMandel.PNG");
         }
 
-        private void Form1_ResizeEnd(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            if (currentSize != Size)
-                Calculate();
-        }
-
-        private void Form1_ResizeBegin(object sender, EventArgs e)
-        {
-            currentSize = Size;
+            Calculate();
+            MessageBox.Show("En grandemandel väntar på dig!");
+            System.Diagnostics.Process.Start("explorer.exe", @"Grandemandel.PNG");
+            this.Close();
         }
     }
 }
